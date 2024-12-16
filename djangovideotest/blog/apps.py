d4732b842bba0c1d3669228a501a4df7cc8b3bfc
hzm74 <hvneerven@protonmail.com>
2024-12-16 22:21:41 +0100

    change apps.py in blog folder

--- djangovideotest/blog/apps.py
+++ djangovideotest/blog/apps.py
@@ -4,3 +4,8 @@ from django.apps import AppConfig
 class BlogConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'blog'
+
+    def ready(self):
+        # Import the model here to ensure it's registered with the app
+        from .video_page import VideoPage
+        
